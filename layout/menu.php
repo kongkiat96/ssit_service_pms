3e1e5693059a5ae01329a587a007237b2dfb2681
Kongkiat <kongkiat.b@srisawadpower.com>
2023-08-14 18:41:43 +0700

    link

--- layout/menu.php
+++ layout/menu.php
@@ -2,7 +2,7 @@
 <div class="container-xxl d-flex h-100">
     <ul class="menu-inner">
         <li class="menu-item <?php echo $page == 'index' ? 'active' : ''; ?>">
-            <a href="index" class="menu-link">
+            <a href="index.php" class="menu-link">
                 <i class="menu-icon tf-icons bx bx-home"></i> ตรวจสอบรายการห้องพัก
             </a>
         </li>
